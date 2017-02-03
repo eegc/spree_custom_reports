48b26961ebc7c5cc03459d7ca54dfa96f7bfc09d
Enrique <engarcia@acid.cl>
2017-02-03 16:42:55 -0300

    Fix query

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -3,9 +3,10 @@ Spree::Variant.class_eval do
   scope :complete_order, -> { joins(:orders).where.not(spree_orders: { completed_at: nil }) }
 
   def self.sales_sku
-    select("spree_variants.id, spree_variants.sku, spree_products.name, spree_variants.variant_name, SUM(spree_line_items.quantity) AS quantity, spree_prices.amount").
-    joins(:product, :default_price).
+    select("spree_variants.id, spree_products.id AS product_id, spree_variants.sku, spree_products.name, spree_variants.variant_name, SUM(spree_line_items.quantity) AS quantity, SUM(spree_line_items.price) AS amount").
+    joins(:product).
     complete_order.
-    group("spree_variants.id, spree_variants.sku, spree_products.name, spree_prices.amount, spree_variants.variant_name")
+    group("spree_variants.id, spree_products.id, spree_variants.sku, spree_products.name, spree_variants.variant_name").order("amount DESC")
   end
+
 end
