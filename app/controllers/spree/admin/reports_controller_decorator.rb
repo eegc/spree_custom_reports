307dee2daecab88c480cd1627cf3cd45cb4c2c8b
Enrique <engarcia@acid.cl>
2017-02-03 16:54:43 -0300

    Add btn for export to csv

--- app/controllers/spree/admin/reports_controller_decorator.rb
+++ app/controllers/spree/admin/reports_controller_decorator.rb
@@ -2,7 +2,10 @@ Spree::Admin::ReportsController.class_eval do
   before_filter :add_sales_sku, only: :index
 
   def sales_sku
-    @item_sales = Spree::Variant.sales_sku.page(params[:page]).per(20)
+    respond_to do |format|
+      format.html { @item_sales = Spree::Variant.sales_sku.page(params[:page]).per(20) }
+      format.csv  { send_data Spree::Variant.sales_sku_csv, filename: "#{Spree.t(:sales_sku).parameterize}-#{Date.today}.csv" }
+    end
   end
 
   private
--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -9,4 +9,20 @@ Spree::Variant.class_eval do
     group("spree_variants.id, spree_products.id, spree_variants.sku, spree_products.name, spree_variants.variant_name").order("amount DESC")
   end
 
+  def self.sales_sku_csv
+    CSV.generate(col_sep: ';', encoding: 'UTF-8') do |csv|
+      csv << ["Codigo/SKU", "Nombre", "Items Vendidos", "Monto sin IVA"]
+
+      sales_sku.each do |item|
+        values =[]
+
+        values << item[:sku]
+        values << (item[:variant_name] || item[:name])
+        values << item[:quantity]
+        values << Spree::Money.new((item[:amount] / 1.19), currency: 'CLP').to_s
+
+        csv << values
+      end
+    end
+  end
 end
