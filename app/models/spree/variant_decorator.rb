fa6648884fb3ff2f6a4fa73fc066fcfd076577c1
Enrique <engarcia@acid.cl>
2017-02-06 15:23:44 -0300

    Add methods for get data and generate csv

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -25,4 +25,29 @@ Spree::Variant.class_eval do
       end
     end
   end
+
+  def self.variant_data
+    select('spree_variants.id, spree_products.id AS product_id, spree_variants.sku, spree_variants.deleted_at, spree_products.available_on, spree_products.deleted_at AS product_deleted_at, ARRAY_AGG(spree_taxons.name) AS taxons, spree_products.description, spree_product_properties.value AS brand').
+    joins(product: [:taxons, :properties]).
+    where(spree_properties: { name: 'brand' }).
+    group('spree_variants.id, spree_products.id, spree_variants.sku, spree_variants.deleted_at, spree_products.description, spree_product_properties.value, spree_products.available_on, spree_products.deleted_at')
+  end
+
+  def self.variant_data_csv
+    CSV.generate(col_sep: ';', encoding: 'UTF-8') do |csv|
+      csv << ["Codigo/SKU", "Categoría", "Descripción", "Marca", "Estado"]
+
+      variant_data.each do |item|
+        values =[]
+
+        values << item[:sku]
+        values << item[:taxons].join(', ')
+        values << ActionController::Base.helpers.strip_tags(item[:description])
+        values << item[:brand]
+        values << Spree.t("available.#{!(item[:available_on].nil? || item[:available_on].future?) && item[:deleted_at].nil? && item[:product_deleted_at].nil?}")
+
+        csv << values
+      end
+    end
+  end
 end
