cf4553e7af857a376ff6bc868becfb36fea524ef
Enrique <engarcia@acid.cl>
2017-02-06 15:22:36 -0300

    Add product details report instance to ReportController

--- app/controllers/spree/admin/reports_controller_decorator.rb
+++ app/controllers/spree/admin/reports_controller_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Admin::ReportsController.class_eval do
-  before_filter :add_sales_sku, only: :index
+  before_filter :add_custom_reports, only: :index
 
   def sales_sku
     respond_to do |format|
@@ -8,9 +8,17 @@ Spree::Admin::ReportsController.class_eval do
     end
   end
 
+  def products_details
+    respond_to do |format|
+      format.html { @items = Spree::Variant.variant_data.page(params[:page]).per(20) }
+      format.csv  { send_data Spree::Variant.variant_data_csv, filename: "#{Spree.t(:sales_sku).parameterize}-#{Date.today}.csv" }
+    end
+  end
+
   private
 
-  def add_sales_sku
+  def add_custom_reports
     Spree::Admin::ReportsController.add_available_report!(:sales_sku)
+    Spree::Admin::ReportsController.add_available_report!(:products_details)
   end
 end
